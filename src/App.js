fa907417e0012c5af2547253516447c12838ac53
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 12:35:05 +0100

    h1 commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+const name = 'Rejoice'
 
 const App =()  => {
   return (
     <div className="App">
-      <h1>Hello, Praise</h1>
+      <h1>Hello, {name}</h1>
     </div>
   );
 }
