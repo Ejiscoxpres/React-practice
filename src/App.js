3c0fb05e9bf5c8dfdeea88a8abbd7d769983f085
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 17:08:16 +0100

    alert commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 /*import Person from './components/person';*/
 
 
 const App =()  => {
 const [counter,setCounter] = useState(0);
+
+useEffect(() => {
+alert('please, refresh');
+})
 
   return (
     <div className="App">
