da770a83fd0ff31d576f8b5ab67bd665d1b5f47e
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 15:27:18 +0100

    components/props commit

--- src/App.js
+++ src/App.js
@@ -16,10 +16,18 @@ const App =()  => {
      {/* {person ()}*/}
       
       
-      <Person name = {'Praise'} lastName = {'Emeka'} age ={500}/> 
-      <Person name = {'Great'} lastName = {'Grace'} age ={501}/> 
-      <Person name = {'Precious'} lastName = {'Promise'} age ={502}/> 
-      <Person name = {'Ihechi'} lastName = {'Amaka'} age ={503}/> 
+      <Person name = {'Praise'}
+       lastName = {'Emeka'} 
+       age ={500}/> 
+      <Person name = {'Great'}  
+      lastName = {'Grace'}
+       age ={501}/> 
+      <Person name = {'Precious'} 
+      lastName = {'Promise'}
+      age ={502}/> 
+      <Person name = {'Ihechi'} 
+      lastName = {'Amaka'} 
+      age ={503}/> 
       <Person name = {'Marvellous'} lastName = {'Munachi'} age ={505}/> 
       
       
