30ea1c5f0303ad09fff736a9054f16dd297ed0f3
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 16:10:07 +0100

    conuter commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const [counter,setCounter] = useState(0);
       
       <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
       <h1>{counter}</h1>
-      <button>+</button>
+      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
       
       
     </div>
