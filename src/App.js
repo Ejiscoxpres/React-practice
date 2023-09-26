7fb6f218659bc0344b8e33a762dabae348fd3a53
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 15:53:56 +0100

    conuter commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
+import {useState} from 'react';
 import './App.css';
-import Person from './components/person';
+/*import Person from './components/person';*/
 
-const person = () => {
-  return(
-    <>
-    <h1>Name: bamidele</h1>
-    <h2>Last Name: Adeleke</h2>
-    <h2>Age: 31</h2>
-    </>
-  )
-}
+
 const App =()  => {
   return (
     <div className="App">
      {/* {person ()}*/}
       
-      
-      <Person name = {'Praise'}
-       lastName = {'Emeka'} 
-       age ={500}/> 
-      <Person name = {'Great'}  
-      lastName = {'Grace'}
-       age ={501}/> 
-      <Person name = {'Precious'} 
-      lastName = {'Promise'}
-      age ={502}/> 
-      <Person name = {'Ihechi'} 
-      lastName = {'Amaka'} 
-      age ={503}/> 
-      <Person name = {'Marvellous'} lastName = {'Munachi'} age ={505}/> 
+      <button>-</button>
+      <h1>0</h1>
+      <button>+</button>
       
       
     </div>
