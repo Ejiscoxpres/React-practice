608581e4840b83c2ce909d75c1ff50bdb5650a74
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 13:00:10 +0100

    testcommit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ const App =()  => {
   return (
     <div className="App">
       <h1>Hello {7+7}!</h1>
+      {name ?(
+        <>
+        test
+        </>
+      ):(
+        test
+      )}
     </div>
   );
 }
