f37ac6264d93794746660b292a01a2344f2a72b2
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 17:54:26 +0100

    new app commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import {useState, useEffect} from 'react';
-import './App.css';
-/*import Person from './components/person';*/
+import React from 'react';
 
+const App = () =>{
+    return(
+        <h1>App</h1>
+    );
 
-const App =()  => {
-const [counter,setCounter] = useState(0);
-
-useEffect(() => {
-alert('you just changed the counter to' + counter);
-}, [counter]);
-
-  return (
-    <div className="App">
-     {/* {person ()}*/}
-      
-      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
-      
-      
-    </div>
-  );
 }
 
 export default App;
--- src/components/person.js
+++
@@ -1,9 +0,0 @@
-export  default function person(props)  {
-    return(
-      <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-      </>
-    )
-  }
