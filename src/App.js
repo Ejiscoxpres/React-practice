d15e656db15764a4f0d6dcb61cb204c461c25e05
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-26 12:54:17 +0100

    simple arithmetic commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const isNameShowing = false;
 const App =()  => {
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing?  name :'peter'}!</h1>
+      <h1>Hello {7+7}!</h1>
     </div>
   );
 }
