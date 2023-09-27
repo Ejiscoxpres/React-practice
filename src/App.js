c64a7155276703ff2cce900a8f1eec0a3d532472
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-27 10:27:13 +0100

    css style commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import {useEffect} from 'react';
+import './App.css';
 
 // dcbabb41
 
 const API_URL = 'http://www.omdbapi.com?apikey=dcbabb41';
 const App = () =>{
-const searchMovie = async (title) => {
+const searchMovies = async (title) => {
 const response = await fetch(`${API_URL}&s=${title}`);
 const data = await response.json();
-console.log(data);
+console.log(data.search);
 }
     useEffect(() =>{
+        searchMovies('stranded');
 
     },[]);
     return(
