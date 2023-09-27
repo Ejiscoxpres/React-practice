c230c11054a75ada36c31882e50391bb1fb27f46
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-27 10:04:12 +0100

    movie api key generator commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import {useEffect} from 'react';
 
+// dcbabb41
+
+const API URL = 'http://www.omdbapi.com?apikey=dcbabb41';
 const App = () =>{
+const searchMovie = async (title) => {
+const response = await fetch(`${API_URL}&s=${title}`);
+const data = await response.json();
+console.log(data);
+}
+    useEffect(() =>{
+
+    },[]);
     return(
         <h1>App</h1>
     );
