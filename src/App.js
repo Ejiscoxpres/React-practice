3f8a57f716075ebd1a887737a9b3f2280b698bb6
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-27 15:34:27 +0100

    moviecard commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {useEffect} from 'react';
+import {useState,useEffect} from 'react';
 import './App.css';
 import SearchIcon from './Search.svg';
+import MovieCard from './MovieCard';
 
 // dcbabb41
 
@@ -18,10 +19,15 @@ const movie1 = {
 }
 
 const App = () =>{
-const searchMovies = async (title) => {
-const response = await fetch(`${API_URL}&s=${title}`);
-const data = await response.json();
-console.log(data.search);
+    const [movies, setMovies] = useState([]);
+     
+     const searchMovies = async (title) => {
+
+      const response = await fetch(`${API_URL}&s=${title}`);
+
+       const data = await response.json();
+
+setMovies(data.search);
 }
     useEffect(() =>{
         searchMovies('stranded');
@@ -43,22 +49,21 @@ console.log(data.search);
                  
                 />
             </div>
-            <div className='container'>
-                <div className='movie'>
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-                    <div>
-                        <img src= {movie1.Poster !== 'N/A' ? movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title} />
-                    </div>
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-
+            {
+                movies?.length > 0 
+                ?  (
+                    <div className='container'>
+                    <MovieCard movie1={movie1}/>
+    
                 </div>
-
-            </div>
+                ):(
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+                
+            }
+            
         </div>
     );
 
